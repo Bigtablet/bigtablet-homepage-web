f5ac1ead0e36a2dbc452fd60b2947105eca263bd
상만 <a01071397193@gmail.com>
2025-10-01 17:22:07 +0900

    feat: solution section

---
+++ src/components/main/problem/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import "./style.scss";
+import {useTranslations} from "next-intl";
+import {usePathname} from "next/navigation";
+
+const Problem = () => {
+    const t = useTranslations("main.problem");
+    const pathname = usePathname();
+    const locale = pathname.split("/")[1] || "ko";
+
+    return (
+        <div className="problem">
+            <span>{t("title")}</span>
+            <span>{t("description")}</span>
+        </div>
+    )
+}
+
+export default Problem;

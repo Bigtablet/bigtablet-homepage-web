ca54b665d439230348d7f41b77597203af4d642e
상만 <a01071397193@gmail.com>
2025-10-14 11:09:49 +0900

    fix: collaborations logo marquee logic update

--- src/components/main/collaborations/index.tsx
+++ src/components/main/collaborations/index.tsx
@@ -1,10 +1,38 @@
-import "./style.scss";
-import { getCollaborationLogos } from "src/utils/collaborations/getLogo";
-import Marquee from "./marquee.client";
+"use client";
 
-const Collaborations = () => {
-    const logos = getCollaborationLogos();
-    return <Marquee logos={logos} speedSec={2} />;
+import Marquee from "react-fast-marquee";
+import Image from "next/image";
+import "./style.scss";
+
+// 로고 이미지 추가는 여기서 해주세요
+const logos = [
+    "/images/collaborations/aws.png",
+    "/images/collaborations/bigtablet.png",
+
+];
+
+const Collaborations = ({ speed = 40 }: { speed?: number }) => {
+    return (
+        <section className="collabs">
+            <h3 className="collabs__title">Collaborations</h3>
+            <div className="collabs__viewport">
+                <Marquee gradient={false} speed={speed} pauseOnHover autoFill>
+                    {logos.map((src, i) => (
+                        <div className="collabs__card" key={`${src}-${i}`}>
+                            <Image
+                                src={src}
+                                alt=""
+                                width={260}
+                                height={80}
+                                priority={i < 4}
+                                draggable={false}
+                            />
+                        </div>
+                    ))}
+                </Marquee>
+            </div>
+        </section>
+    );
 };
 
 export default Collaborations;

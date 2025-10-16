60593cdd531b86792737f730671733937cbef39d
상만 <a01071397193@gmail.com>
2025-10-16 15:51:39 +0900

    feat: gcp

--- src/queries/queryKey.ts
+++ src/queries/queryKey.ts
@@ -2,5 +2,9 @@ export const QueryKey = {
     recruit: {
         list: "/recruit",
         detail: "/recruit/detail",
+        apply: "/recruit/apply",
     },
+    gcp: {
+        upload: "/gcp"
+    }
 }

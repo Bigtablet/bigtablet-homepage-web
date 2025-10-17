9463378f2c63a8a640bc7d48483ca3e37ecaf670
상만 <a01071397193@gmail.com>
2025-10-17 15:03:22 +0900

    delete: locale

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import {redirect} from 'next/navigation';
-import {defaultLocale} from 'src/i18n/navigation';
 
 export default function Page() {
-    redirect(`/${defaultLocale}/main`);
+    redirect(`en/main`);
 }

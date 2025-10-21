23346bcfbeeb0e61e003dac5d35d2ecbbda8ed32
상만 <a01071397193@gmail.com>
2025-10-21 17:34:49 +0900

    fix: about top style

--- src/components/about/introduce/index.tsx
+++ src/components/about/introduce/index.tsx
@@ -1,39 +1,22 @@
 "use client";
 
-import React from "react";
+import "./style.scss";
 import { useTranslations } from "next-intl";
 import type { AboutType } from "src/types/about/introduce.type";
-import "./style.scss";
 
 const Introduce = ({ sectionKey, reverse = false }: AboutType) => {
     const t = useTranslations("about.top");
 
     return (
-        <div className="about">
-            {!reverse && (
-                <>
-                    <div className="about__text">
-                        <p className="title">{t(`${sectionKey}.title`)}</p>
-                        <p className="description">{t(`${sectionKey}.description`)}</p>
-                    </div>
-                    <div className="about__image">
-                        <img src="/images/logo/logo.png" alt="logo" />
-                    </div>
-                </>
-            )}
-
-            {reverse && (
-                <>
-                    <div className="about__image">
-                        <img src="/images/logo/logo.png" alt="logo" />
-                    </div>
-                    <div className="about__text about__text--reverse">
-                        <p className="title">{t(`${sectionKey}.title`)}</p>
-                        <p className="description">{t(`${sectionKey}.description`)}</p>
-                    </div>
-                </>
-            )}
-        </div>
+        <section className={`about ${reverse ? "about--reverse" : ""}`}>
+            <div className="about__text">
+                <p className="about__title">{t(`${sectionKey}.title`)}</p>
+                <p className="about__desc">{t(`${sectionKey}.description`)}</p>
+            </div>
+            <div className="about__image" aria-hidden="true">
+                <img src="/images/logo/logo.png" alt="logo" />
+            </div>
+        </section>
     );
 };
 

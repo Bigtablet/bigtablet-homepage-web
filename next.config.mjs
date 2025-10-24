12df4a63ad4072d7660ac45668012fe79d9c56d8
상만 <a01071397193@gmail.com>
2025-10-24 11:31:41 +0900

    fix: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin({});
 
 const nextConfig = {
     images: { domains: ['storage.googleapis.com'] },
+
+    async redirects() {
+        return [
+            { source: '/', destination: '/main', permanent: true },
+        ];
+    },
 };
 
 export default withNextIntl(nextConfig);

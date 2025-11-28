a07d3048c829a9a2d4a1403bdd3831df06939019
상만 <a01071397193@gmail.com>
2025-11-28 13:34:42 +0900

    feat: recruit benefit

--- src/app/(i18n)/recruit/[idx]/page.tsx
+++ src/app/(i18n)/recruit/[idx]/page.tsx
@@ -23,7 +23,7 @@ const formatDate = (s?: string) => {
     return `${y}.${m}.${day}`;
 };
 
-const BENEFITS: string[] = ["Benefit 1", "Benefit 2", "Benefit 3", "Benefit 4", "Benefit 5", "Benefit 6"];
+const BENEFITS: string[] = ["업무에 필요한 장비 (Mac, Window, 키보드, 마우스, 모니터 등) 지원.", "시차 출퇴근", "간식 창고 자유롭게 이용 가능", "수평적인 구조, 상호 존중으로 ~님 호칭을 기본적으로 사용", "필요한 AI툴 지원 (기본적으로 GPT5 지원)", "점심 식대 지원, 야근 시 저녁 식대 지원."];
 const HIRING_PROCESS: string[] = ["서류 전형", "1차 면접", "2차 면접 (필요시)", "처우 협의", "합류"];
 
 const RecruitDetail = () => {

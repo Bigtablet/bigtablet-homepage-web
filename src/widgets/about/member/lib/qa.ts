3de4316c038f43dac5f3099eefcf95702e46c298
상만 <a01071397193@gmail.com>
2025-12-09 18:23:36 +0900

    delete: member folder

--- src/widgets/about/member/lib/qa.ts
+++ src/widgets/about/member/lib/qa.ts
@@ -1,4 +1,4 @@
-import {MemberKey} from "src/entities/about/member/model/util/member.util";
+import {MemberKey} from "src/entities/about/model/util/member.util";
 
 
 export interface QAItem { q: string; a: string }

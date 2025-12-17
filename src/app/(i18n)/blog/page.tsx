ae8d94ed9ab4d4ac32716e0b7ed1527117eaab2e
상만 <a01071397193@gmail.com>
2025-12-17 13:36:32 +0900

    bug: fix blog list section

--- src/app/(i18n)/blog/page.tsx
+++ src/app/(i18n)/blog/page.tsx
@@ -2,42 +2,51 @@
 
 import { useMemo } from "react";
 import { useLocale } from "next-intl";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import Template from "src/shared/ui/template";
 import { useBlogPageQuery } from "src/entities/blog/queries/blog.query";
 import BlogListSection from "src/widgets/blog/list";
-import { Pagination } from "src/shared/ui/navigation/pagination";
+import {Pagination} from "@bigtablet/design-system";
+import "./style.scss";
 
-const DEFAULT_SIZE = 9;
+const DEFAULT_SIZE = 6;
 
 const BlogPage = () => {
     const locale = useLocale();
     const pathname = usePathname();
     const sp = useSearchParams();
+    const router = useRouter();
 
+    const page = Math.max(1, Number(sp.get("page") ?? 1));
     const size = Math.max(1, Number(sp.get("size") ?? DEFAULT_SIZE));
 
-    const { data, isLoading } = useBlogPageQuery({ page: 1, size: 6 });
+    const { data, isLoading } = useBlogPageQuery({ page, size });
 
-    const items = useMemo(() => data?.items ?? [], [data]);
+    const items = useMemo(() => data?.items ?? [], [data?.items]);
+
+    const totalPages = data?.hasNext ? page + 1 : page;
+
+    const handlePageChange = (nextPage: number) => {
+        const params = new URLSearchParams(sp.toString());
+        params.set("page", String(nextPage));
+        params.set("size", String(size));
+        router.push(`${pathname}?${params.toString()}`);
+    };
 
     return (
         <Template align="center">
             <div className="blog-page">
-                <Pagination items={items} pageSize={size} maxPageButtons={7}>
-                    {(pageItems) => (
-                        <BlogListSection
-                            items={pageItems}
-                            locale={locale}
-                            isLoading={isLoading}
-                            hasNextPage={false}
-                            isFetchingNextPage={false}
-                            fetchNextPage={() => {}}
-                            pageSize={size}
-                            hrefBuilder={(item) => `${pathname}/${item.idx}`}
-                        />
-                    )}
-                </Pagination>
+                <BlogListSection
+                    items={items}
+                    locale={locale}
+                    isLoading={isLoading}
+                    pageSize={size}
+                    hrefBuilder={(item) => `${pathname}/${item.idx}`}
+                />
+
+                {totalPages > 1 && (
+                    <Pagination page={page} totalPages={totalPages} onChange={handlePageChange} />
+                )}
             </div>
         </Template>
     );
